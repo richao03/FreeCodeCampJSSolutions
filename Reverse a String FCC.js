88347ab5f97e7b4ed8c564c2740cc8953f22653b
Richard <r.chao03@gmail.com>
2016-03-24 01:16:36 -0400

    Update Reverse a String FCC.js

--- Reverse a String FCC.js
+++ Reverse a String FCC.js
@@ -1,7 +1,17 @@
-function reverseString(str) {
-  
-  return str.split("").reverse().join(""); //there are basic methods of str and array, if these seem unfamiliar, please take your time 
-                                           //and go through every single method for str, arr, and numbers on MDN
-}
+/* Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.
+Your result must be a string.
+*/
 
+function reverseString(str) {
+  return str.split("").reverse().join(""); //there are basic methods of str and array, if these seem unfamiliar, please take time 
+                                           //to go through every single method for str, arr, and numbers on MDN
+}
 reverseString("hello");  // should return "olleh"
+
+/* test cases: 
+reverseString("hello") should return a string.
+reverseString("hello") should become "olleh".
+reverseString("Howdy") should become "ydwoH".
+reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+*/
